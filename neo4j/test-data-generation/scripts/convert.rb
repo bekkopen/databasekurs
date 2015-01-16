4a2cd0aa0b0603a5be2035ab7e0574309302eef8
Jørgen <jorgen.braseth@gmail.com>
2015-01-16 16:14:01 +0100

    Lagt til testdata med riktig encoding som JSON og CSV

--- neo4j/test-data-generation/scripts/convert.rb
+++ neo4j/test-data-generation/scripts/convert.rb
@@ -5,10 +5,11 @@ require 'json'
 require './dump_data'
 
 def csv_as_objects(filename)
-  items = []
-  lines = CSV.read(filename,'r')
+  lines = CSV.open(filename, 'r:utf-8')
+
   header = lines.shift
 
+  items = []
   lines.each do |line|
     items << Hash[header.zip line]
   end
--- neo4j/test-data-generation/scripts/dump_data.rb
+++ neo4j/test-data-generation/scripts/dump_data.rb
@@ -5,7 +5,7 @@ require 'json'
 
 def dump_to_csv(items, filename = 'data.csv')
   puts "Dumping #{items.size} items as CSV to #{filename}"
-  CSV.open(filename, 'w') do |csv|
+  CSV.open(filename, 'w:utf-8') do |csv|
     csv << items.first.keys
     items.each do |user|
       csv << (user.values)
@@ -15,7 +15,7 @@ end
 
 def dump_to_json(items, filename = 'data.json')
   puts "Dumping #{items.size} items as JSON to #{filename}"
-    File.open(filename, 'w') do |file|
+    File.open(filename, 'w:utf-8') do |file|
       file << items.to_json
     end
 end
